7927ef92b6ea630d400088c2fe11f515486c8b54
Seu Nome ou Usuario Do Github <seu-email@exemplo.com>
2026-08-10 21:22:52 -0300

    Corrige SyntaxError no server.js

--- src/web/server.js
+++ src/web/server.js
@@ -25,14 +25,14 @@ module.exports = (client) => {
 
     app.get('/', (req, res) => {
         const session = sessions[req.cookies?.sessionId];
-        const inviteUrl = \`https://discord.com/oauth2/authorize?client_id=\${CLIENT_ID}&scope=bot%20applications.commands&permissions=8\`;
+        const inviteUrl = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=bot%20applications.commands&permissions=8`;
         res.send(renderHome(session?.user || null, client.user, inviteUrl, SUPPORT_URL));
     });
 
     app.get('/login', (req, res) => {
         if (!CLIENT_ID) return res.status(500).send('CLIENT_ID não configurado.');
         const encodedRedirect = encodeURIComponent(REDIRECT_URI);
-        const url = \`https://discord.com/oauth2/authorize?client_id=\${CLIENT_ID}&redirect_uri=\${encodedRedirect}&response_type=code&scope=identify%20guilds\`;
+        const url = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodedRedirect}&response_type=code&scope=identify%20guilds`;
         res.redirect(url);
     });
 
@@ -57,10 +57,10 @@ module.exports = (client) => {
             if (!tokenData.access_token) return res.redirect('/');
 
             const userRes = await fetch('https://discord.com/api/users/@me', {
-                headers: { authorization: \`\${tokenData.token_type} \${tokenData.access_token}\` },
+                headers: { authorization: `${tokenData.token_type} ${tokenData.access_token}` },
             });
             const guildsRes = await fetch('https://discord.com/api/users/@me/guilds', {
-                headers: { authorization: \`\${tokenData.token_type} \${tokenData.access_token}\` },
+                headers: { authorization: `${tokenData.token_type} ${tokenData.access_token}` },
             });
 
             const user = await userRes.json();
@@ -109,7 +109,7 @@ module.exports = (client) => {
             botName: client.user.username,
             botAvatarUrl: client.user.displayAvatarURL(),
             userAvatarUrl: session.user.avatar
-                ? \`https://cdn.discordapp.com/avatars/\${session.user.id}/\${session.user.avatar}.png\`
+                ? `https://cdn.discordapp.com/avatars/${session.user.id}/${session.user.avatar}.png`
                 : 'https://cdn.discordapp.com/embed/avatars/0.png'
         }));
     });
@@ -130,7 +130,7 @@ module.exports = (client) => {
             : [];
 
         const userAvatarUrl = session.user.avatar
-            ? \`https://cdn.discordapp.com/avatars/\${session.user.id}/\${session.user.avatar}.png\`
+            ? `https://cdn.discordapp.com/avatars/${session.user.id}/${session.user.avatar}.png`
             : 'https://cdn.discordapp.com/embed/avatars/0.png';
 
         const config = db.getGuildConfig(guild.id);
@@ -163,6 +163,6 @@ module.exports = (client) => {
     });
 
     app.listen(PORT, () => {
-        console.log(\`🌐 Painel Web rodando na porta \${PORT}\`);
+        console.log(`🌐 Painel Web rodando na porta ${PORT}`);
     });
 };
