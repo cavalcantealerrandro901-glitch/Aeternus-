15d40f1118a7dd3608dc27ce0c79f88ad1b4b17f
cavalcantealerrandro901-glitch <cavalcantealerrandro901@gmail.com>
2026-08-29 17:02:10 -0300

    Saldo/atm/bal so flocos e cristais + dica loja

--- commands/saldo.js
+++ commands/saldo.js
@@ -1,24 +1,36 @@
 const { EmbedBuilder } = require('discord.js');
 const flocos = require('../utils/flocos');
 const cristais = require('../utils/cristais');
-const xp = require('../utils/xp');
-const bank = require('../utils/bank');
 
 module.exports = {
     name: 'saldo',
-    aliases: ['bal', 'atm', 'balance'],
+    aliases: ['bal', 'atm', 'balance', 'carteira'],
+    description: 'Mostra flocos e cristais',
     async execute(message) {
         const user = message.mentions.users.first() || message.author;
-        const x = xp.get(user.id);
+        const f = flocos.get(user.id);
+        const c = cristais.get(user.id);
+
         await message.reply({
             embeds: [
                 new EmbedBuilder()
                     .setColor(0x8b5cf6)
-                    .setAuthor({ name: user.username, iconURL: user.displayAvatarURL() })
+                    .setAuthor({
+                        name: user.username,
+                        iconURL: user.displayAvatarURL({ size: 64 })
+                    })
                     .setTitle('💎 Carteira')
                     .setDescription(
-                        `${flocos.format(flocos.get(user.id))}\n🏦 Banco: **${flocos.formatPlain(bank.get(user.id))}**\n${cristais.format(cristais.get(user.id))}\n⭐ Nível **${x.level}**`
+                        [
+                            `❄️ **${flocos.formatPlain(f)}** flocos`,
+                            `💠 **${cristais.formatPlain(c)}** cristais`,
+                            '',
+                            '🛒 Você pode usar o comando **`/loja cristais`** para comprar itens.'
+                        ].join('\n')
                     )
+                    .setThumbnail(user.displayAvatarURL({ size: 128 }))
+                    .setFooter({ text: 'Aeternus · economia' })
+                    .setTimestamp()
             ]
         });
     }
